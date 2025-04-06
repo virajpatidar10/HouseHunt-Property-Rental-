e34b77cdc2401a86059cfe50dc8057e20ce15737
Anukul0317 <anukul0317@gmail.com>
2025-04-06 14:48:40 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Navbar from "../components/Navbar"
+import Slide from "../components/Slide"
+import Categories from "../components/Categories"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Navbar />
+      <Slide />
+      <Categories />
+    </>
   )
 }
 
