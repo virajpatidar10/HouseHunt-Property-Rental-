285690ed78470a04e0533cd66846515eb1cf64c2
virajpatidar10 <142742908+virajpatidar10@users.noreply.github.com>
2025-04-06 23:09:46 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
-import Navbar from "../components/Navbar";
-import Slide from "../components/Slide";
-import Categories from "../components/Categories";
+import Navbar from "../components/Navbar"
+import Slide from "../components/Slide"
+import Categories from "../components/Categories"
+import Listings from "../components/Listings"
+import Footer from "../components/Footer"
 
 const HomePage = () => {
   return (
@@ -8,8 +10,10 @@ const HomePage = () => {
       <Navbar />
       <Slide />
       <Categories />
+      <Listings />
+      <Footer />
     </>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
