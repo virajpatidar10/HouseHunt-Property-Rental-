e66f6fcaf1cb6b9779c622414fe571eb4bb7a0c3
Anukul0317 <anukul0317@gmail.com>
2025-04-06 13:46:59 +0530

    Added Home page and Login using JWT && Created create listing functionality

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,12 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 
-const User = require("../models/User.js");
+const User = require("../models/User");
+
+/* Configuration Multer for File Upload */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/uploads/"); 
+    cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Use the original file name
@@ -14,35 +16,83 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* USER REGISTER */
 router.post("/register", upload.single("profileImage"), async (req, res) => {
   try {
-    const { firstname, lastname, email, password } = req.body;
+    /* Take all information from the form */
+    const { firstName, lastName, email, password } = req.body;
 
-    const profileimage = req.file;
+    /* The uploaded file is available as req.file */
+    const profileImage = req.file;
 
-    if (!profileimage) {
+    if (!profileImage) {
       return res.status(400).send("No file uploaded");
     }
-    const profileImagePath = profileimage.path;
 
+    /* path to the uploaded profile photo */
+    const profileImagePath = profileImage.path;
+
+    /* Check if user exists */
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(409).json({ message: "User already exists!" });
     }
+
+    /* Hass the password */
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
+
+    /* Create a new User */
     const newUser = new User({
-      firstname,
-      lastname,
+      firstName,
+      lastName,
       email,
       password: hashedPassword,
       profileImagePath,
     });
+
+    /* Save the new User */
     await newUser.save();
-    res.status(200).json({ message: "User registered successfully!", user: newUser });
+
+    /* Send a successful message */
+    res
+      .status(200)
+      .json({ message: "User registered successfully!", user: newUser });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Registration failed!", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Registration failed!", error: err.message });
+  }
+});
+
+/* USER LOGIN*/
+router.post("/login", async (req, res) => {
+  try {
+    /* Take the infomation from the form */
+    const { email, password } = req.body;
+
+    /* Check if user exists */
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(409).json({ message: "User doesn't exist!" });
+    }
+
+    /* Compare the password with the hashed password */
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials!" });
+    }
+
+    /* Generate JWT token */
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+
+    res.status(200).json({ token, user });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
   }
 });
 
